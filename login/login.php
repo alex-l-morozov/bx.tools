bf7997ed6bd2d1ee49dfdcee06099a69ed54745c
Alexander Morozov <mail@alexandermorozov.ru>
2017-01-11 23:12:21 +0300

    login.php

--- login/login.php
+++ login/login.php
@@ -6,16 +6,37 @@ define("NO_KEEP_STATISTIC", "Y");
 // очистка устаревших данных статистики;
 // отсылка ежедневного статистического отчета.
 define("NO_AGENT_STATISTIC","Y");
+// Битрикс24:
+// Отключение проверки прав на доступ к файлам и каталогам
+// define("NOT_CHECK_PERMISSIONS", true);
 
 // Подключение служебной части пролога
 require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
 
 global $USER;
-// Авторизация пользователя первого пользователя (администратор)
-$USER->Authorize(1);
+
+// Авторизация пользователя (администратора)
+$rsUser = $USER->GetList(
+    "ID",
+    "ASC",
+    array(
+        "GROUPS_ID" => array(1)
+    ),
+    array(
+        'FIELDS' => array('ID'),
+        'NAV_PARAMS' => array("nTopCount" => 1)
+    )
+);
+
+if($arUser = $rsUser->Fetch())
+    $USER->Authorize($arUser['ID']);
 
 // Удаляем файл после авторизации пользователя
 @unlink(__FILE__);
 
 // Переадресовываем в панель управления сайтом
 LocalRedirect("/bitrix/admin/");
+// Битрикс24:
+// Переадресовываем на корень
+// LocalRedirect("/");
+?>
