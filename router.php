0c362a04c2ee5f27b4938f2a09609851bf2c17cc
Alexander Morozov <mail@alexandermorozov.ru>
2025-09-21 18:02:32 +0300

    Create router.php

---
+++ router.php
@@ -0,0 +1,31 @@
+<?php
+use Bitrix\Main\ModuleManager;
+use Bitrix\Main\Routing\RoutingConfigurator;
+
+$getRoutePaths = static function (): array {
+    $prefixes = ['mymodule'];
+    $routes = [];
+
+    foreach (ModuleManager::getInstalledModules() as $module) {
+        foreach ($prefixes as $prefix) {
+            if (str_starts_with($module['ID'], $prefix . '.')) {
+                $route = $_SERVER['DOCUMENT_ROOT'] . '/local/modules/' . $module['ID'] . '/routes/api.php';
+                if (file_exists($route)) {
+                    $routes[] = $route;
+                }
+            }
+        }
+    }
+
+    return $routes;
+};
+
+return function (RoutingConfigurator $routingConfigurator) use ($getRoutePaths) {
+    foreach ($getRoutePaths() as $route) {
+        $callback = include_once $route;
+
+        if ($callback instanceof Closure) {
+            $callback($routingConfigurator);
+        }
+    }
+};
